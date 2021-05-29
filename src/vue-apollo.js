0774e7ace667860a8fcf9c868a1d6c265c6f47bf
Lee, JunHyuk <leejh10003@gmail.com>
2021-05-29 20:49:20 +0900

    Import setContext

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueApollo from 'vue-apollo'
 import { /*createApolloClient, */restartWebsockets } from 'vue-cli-plugin-apollo/graphql-client'
+import { setContext } from '@apollo/link-context'
 import { split } from 'apollo-link'
 import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
