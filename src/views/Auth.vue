f5b33b0bfd0e7a6d6a9f76849a47f206d3a0890b
Lee, JunHyuk <leejh10003@gmail.com>
2021-06-10 17:51:21 +0900

    Add login screen style

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-    Authed
+    
   </div>
 </template>
 <script>
+import axios from 'axios' //eslint-disable-line no-unused-vars
 export default {
-  beforeCreate: function(){
-    console.log(this.$route.query)
+  mounted: async function(){
+    const { code, state } = this.$route.query //eslint-disable-line no-unused-vars
+    const loading = this.$vs.loading({
+      type: 'corners',
+      color: '#2BD400',
+      scale: 4,
+      text: '로그인 중...'
+    })
+    try {
+      const result = await axios.post('https://trello.jeontuk-11.link/login/naver', {
+        code, state
+      })
+      console.log(result)
+    } catch (e){
+      console.error(e)
+      loading.close()
+      this.$router.push('/naverAuth')
+    }
   }
 }
 </script>
