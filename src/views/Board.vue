0615e1933ffe594c406662147d695c7e445a3b4a
SNTDev <lmj960417@gmail.com>
2021-06-10 20:53:12 +0900

    Add Card component

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -33,35 +33,11 @@
           </template>
         </div>
         <draggable class="list-group" :list="list.cards" group="card">
-            <vs-card
+            <div
             v-for="(element) in list.cards"
             :key="element.id">
-              <template #title>
-                <h3>{{ getTitle(element) }}</h3>
-              </template>
-              <template #img>
-                <img :src="element.created_by.thumbnail" alt="">
-              </template>
-              <template #text>
-                <div v-for="(paragraph, index) in element.card_descriptions" :key="index">
-                  <a v-if="!!(paragraph.hyperlink)" :href="paragraph.hyperlink">{{paragraph.content}}</a>
-                  <p v-else-if="!!(paragraph.content)">
-                    {{paragraph.content}}
-                  </p>
-                </div>
-              </template>
-              <template #interactions>
-                <vs-button danger icon>
-                  <i class='bx bx-heart'></i>
-                </vs-button>
-                <vs-button class="btn-chat" shadow primary>
-                  <i class='bx bx-chat' ></i>
-                  <span class="span">
-                    54
-                  </span>
-                </vs-button>
-              </template>
-            </vs-card>
+              <Card v-bind:element="{...element}" />
+            </div>
         </draggable>
       </div>
     </draggable>
@@ -78,13 +54,15 @@
 <script>
 import draggable from "vuedraggable";
 import gql from 'graphql-tag';
+import Card from '../components/Card.vue';
 
 export default {
   name: "two-lists",
   display: "Two Lists",
   order: 1,
   components: {
-    draggable
+    draggable,
+    Card
   },
   apollo: {
     lists: {
