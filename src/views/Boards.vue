c265c8caa944d9788d95eaff8e7d8bad0360fd07
Lee, JunHyuk <leejh10003@gmail.com>
2021-06-12 05:37:16 +0900

    Remove redundant appbar

--- src/views/Boards.vue
+++ src/views/Boards.vue
@@ -2,27 +2,6 @@
   <div v-if="$apollo.queries.boards.loading">
   </div>
   <div v-else>
-    <vs-navbar color="#7d33ff" text-white square center-collapsed v-model="active">
-        <template #left>
-          <img src="/logos/logo-vuesax-logotipo-vuesax-png-4.png" alt="">
-        </template>
-        <vs-navbar-item :active="active == 'guide'" id="guide">
-          Guide
-        </vs-navbar-item>
-        <vs-navbar-item :active="active == 'docs'" id="docs">
-          Documents
-        </vs-navbar-item>
-        <vs-navbar-item :active="active == 'components'" id="components">
-          Components
-        </vs-navbar-item>
-        <vs-navbar-item :active="active == 'license'" id="license">
-          license
-        </vs-navbar-item>
-        <template #right>
-          <vs-button color="#fff" flat >Login</vs-button>
-          <vs-button color="#fff" border >Get Started</vs-button>
-        </template>
-      </vs-navbar>
     <board-card
       :style="{maxHeight: '125px'}"
       v-for="board in boards"
