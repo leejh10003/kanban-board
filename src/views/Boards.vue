126fdb582db6d613d51e6f9049a11c7d29da0d05
Lee, JunHyuk <leejh10003@gmail.com>
2021-06-12 06:24:16 +0900

    Apply responsiveness to boards view

--- src/views/Boards.vue
+++ src/views/Boards.vue
@@ -2,18 +2,40 @@
   <div v-if="$apollo.queries.boards.loading">
   </div>
   <div v-else>
-    <board-card
-      :style="{maxHeight: '125px'}"
-      v-for="board in boards"
-      :key="board.id"
-      :board="board"
-    />
+    <vs-row v-for="(row, index) in splitTwo(boards)" :key="`row${index}`" vs-justify="center">
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" :lg="row.length === 1 ? 4 : 2" :sm="1" :xs="0">
+      </vs-col>
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" lg="4" sm="5" xs="12" v-for="board in row" :key="`board${board.id}`">
+        <board-card
+          :board="board"
+        />
+      </vs-col>
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" :lg="row.length === 1 ? 4 : 2" :sm="1" :xs="0">
+      </vs-col>
+    </vs-row>
+    <!--<vs-row >
+      
+    </vs-row>-->
   </div>
 </template>
 <script>
 import gql from 'graphql-tag'
 import BoardCard from '../components/BoardCard.vue'
+/** */
 export default {
+  methods: {
+    splitTwo(list){
+      var index = 0;
+      var tempArray = [];
+      
+      for (index = 0; index < list.length; index += 2) {
+          var chunk = list.slice(index, index+2);
+          // Do something if you want with the group
+          tempArray.push(chunk);
+      }
+      return tempArray;
+    }
+  },
   components: {
     'board-card': BoardCard
   },
@@ -27,7 +49,6 @@ export default {
   },
   data: function(){
     return {
-      active: 'guide',
       loading: null
     }
   },
