1c62db353a5d3e29d20d3c7db7576a4541c2eedb
Lee, JunHyuk <leejh10003@gmail.com>
2021-06-12 17:40:30 +0900

    Apply edit naming inline

--- src/components/BoardCard.vue
+++ src/components/BoardCard.vue
@@ -4,7 +4,7 @@
     >
       <template #title>
         <h3 v-if="!titleEditing" @click.stop="titleEdit" class="board-title">{{board.name}}</h3>
-        <el-input v-else v-model="board.name" @click.stop="titleEdit" class="board-title"/>
+        <input @keyup.enter="submit" v-else v-model="board.name" @click.stop="titleEdit" class="board-title-edit" placeholder="제목..."/>
       </template>
       <template #tailing>
         <div class="tailing-users">
@@ -21,6 +21,7 @@
     </card>
 </template>
 <script>
+import gql from 'graphql-tag'
 import card from './Card.vue'
 export default {
   data(){
@@ -32,6 +33,20 @@ export default {
     card
   },
   methods: {
+    async submit(){
+      this.titleEditing = false
+      this.$apollo.mutate({
+        variables: {
+          name: this.board.name,
+          id: this.board.id
+        },
+        mutation: gql`mutation($name: String!, $id: Int!){
+          update_boards_by_pk(pk_columns: {id: $id}, _set: {name: $name}){
+            id
+          }
+        }`
+      })
+    },
     titleEdit(){
       this.titleEditing = true
     },
@@ -70,6 +85,17 @@ export default {
 }
 </script>
 <style scoped>
+.board-title-edit{
+  border: 0px solid transparent;
+  display: block;
+  font-size: 1.17em;
+  margin-block-start: 1em;
+  margin-block-end: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: bold;
+  width: 100%;
+}
 .board-title{
   text-overflow: ellipsis;
   white-space: nowrap;
