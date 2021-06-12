60be18ef64bc7f3cbcbf60990ac0ef5f0d0e4048
SNTDev <lmj960417@gmail.com>
2021-06-13 02:17:42 +0900

    Modify to material ui

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -17,52 +17,30 @@
     </template>
     <template #tailing>
       <div class="post-author">
-        <div class="tag-list">
-          <vs-button v-for="(t, index) in element.card_taggings" :key="index" transparent color="primary">
-            {{t.tag.tag}}
-          </vs-button>
-        </div>
-        <div class="tag-edit">
-          <vs-button transparent icon color="dark" @click="active = true">
-            <i class="bx bx-edit"></i>
-          </vs-button>
-        </div>
+        <el-select
+          class="tag-select"
+          v-model="tagIds"
+          multiple
+          filterable
+          allow-create
+          default-first-option
+        >
+          <el-option
+            v-for="item in tagList"
+            :key="item.id"
+            :label="item.tag"
+            :value="item.id"
+          >
+          </el-option>
+        </el-select>
       </div>
-      <vs-dialog v-model="active">
-        <template #header>
-          <h4 class="not-margin">Tag edit</h4>
-        </template>
-        <div class="dialog-tag-list">
-          <div v-for="(t, index) in tagList" :key="index">
-            <vs-button :border="checkTag(t.id) === false" v-on:click="switchTag(t.id)" dark>{{t.tag}} </vs-button>
-          </div>
-        </div>
-         <template #footer>
-          <div class="footer-dialog">
-             <vs-input v-model="newTagName" placeholder="New Tag" />
-            <vs-button @click="addNewTag(newTagName)">
-              Add New Tag
-            </vs-button>
-          </div>
-        </template>
-      </vs-dialog>
     </template>
   </Card>
 </template>
 <style>
-.footer-dialog {
-  display: flex;
-}
-.post-description {
-  margin: 0px;
-}
-.tag-list {
-  display: flex;
-  float: left;
-}
-.tag-edit {
-  display: flex;
-  float: right;
+.el-select {
+  width: 80%;
+  color: black;
 }
 .dialog-tag-list {
   display: flex;
@@ -78,13 +56,13 @@ export default {
   data() {
     return {
       active: false,
-      tagIds: {},
-      newTagName: ""
+      tagIds: [],
+      newTagName: "",
     };
   },
   created() {
-    this.$props.element.card_taggings.forEach(e => {
-      this.$data.tagIds[e.tag.id] = true;
+    this.$data.tagIds = this.$props.element.card_taggings.map((e) => {
+      return e.tag.id;
     });
   },
   props: {
@@ -99,23 +77,16 @@ export default {
         element?.card_descriptions?.[0]?.hyperlink) ??
       "",
     getTagIds() {
-      return this.$props.element.card_taggings.map(e => {
+      return this.$props.element.card_taggings.map((e) => {
         return e.tag.id;
-      })
+      });
     },
-    addNewTag: async function(tagName) {
+    addNewTag: async function (tagName) {
       const boardId = this.$route.params.id;
       await this.$apollo.mutate({
         mutation: gql`
           mutation addTag($board_id: Int!, $tag_name: String!) {
-            insert_tag(
-              objects: [
-                {
-                  board_id: $board_id
-                  tag: $tag_name
-                }
-              ]
-            ) {
+            insert_tag(objects: [{ board_id: $board_id, tag: $tag_name }]) {
               returning {
                 id
               }
@@ -124,7 +95,7 @@ export default {
         `,
         variables: {
           board_id: boardId,
-          tag_name: tagName
+          tag_name: tagName,
         },
       });
 
@@ -136,7 +107,7 @@ export default {
     switchTag(id) {
       this.$data.tagIds[id] = !this.$data.tagIds[id];
       console.log(this.$data.tagIds);
-    }
+    },
   },
 };
 </script>
