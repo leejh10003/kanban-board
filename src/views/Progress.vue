2472a1fc323b7b9efc2d54f7c13e9c8d48c3723e
Lee, JunHyuk <leejh10003@gmail.com>
2021-06-13 20:01:58 +0900

    Get user using boards progress

---
+++ src/views/Progress.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    {{boards}}
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+import { getToken } from '../vue-apollo'
+import gql from 'graphql-tag'
+export default {
+  async beforeCreate(){
+    await getToken();
+    if (this.$store.state.loggedIn){
+      this.$router.push('/boards')
+    } else {
+      this.$router.push('/naverAuth')
+    }
+  },
+  apollo: {
+    boards: {
+      query: gql`query($id: Int!){
+        boards(where: {boards_users: {id: {_eq: $id}}}) {
+          id
+          name
+          progress(where: {user_id: {_eq: $id}}) {
+            board_id
+            progress
+            column {
+              name
+            }
+            finished_num
+            column_id
+            card_num
+          }
+        }
+      }
+      `
+    }
+  }
+}
+</script>
