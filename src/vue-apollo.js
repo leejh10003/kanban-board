0f8913c39656a153b18df7cac172dab50f187d39
Lee, JunHyuk <leejh10003@gmail.com>
2021-06-13 16:20:36 +0900

    Use subscription to use own refreshing

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueApollo from 'vue-apollo'
-//import { setContext } from '@apollo/client/link/context'
 import { split } from 'apollo-link'
 import { WebSocketLink } from 'apollo-link-ws'
 import { getMainDefinition } from 'apollo-utilities'
@@ -8,20 +7,23 @@ import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import ApolloClient from "apollo-client";
 import axios from 'axios'
-import { store } from './vuex-config'
 import jwtDecode from 'jwt-decode'
+import { store } from './vuex-config'
+import { setContext } from '@apollo/link-context'
 import router from './router'
-import { ApolloLink } from '@apollo/client';
-import from from 'core-js/features/array/from';
 Vue.use(VueApollo)
 
-export const refreshToken = async function(){
+export const getToken = async function(){
   try{
-    const result  = await axios.post('https://trello.jeontuk-11.link/refresh')
-    localStorage.setItem('token', result.data.token)
-    const user = jwtDecode(result.data.token)
-    store.commit('login', user)
-    return result.data.token
+    console.log('getToken')
+    const currentToken = localStorage.getItem('token')
+    if (!currentToken || Date(jwtDecode(currentToken)) < Date.new()){
+      const { data: { token } } = await axios.post('https://trello.jeontuk-11.link/refresh')
+      localStorage.setItem('token', token)
+      return token
+    } else {
+      return currentToken
+    }
   } catch(e) {
     localStorage.removeItem('token')
     store.commit('logout')
@@ -30,56 +32,42 @@ export const refreshToken = async function(){
   }
 }
 
-const authMiddleware = new ApolloLink(async (operation, forward) => {
-  console.log('authlink called')
-  const token = localStorage.getItem('token')
+const authLink = setContext(async(_, { headers }) => {
   try {
-    if (store.state.loggedIn === false){
-      store.commit('login', jwtDecode(token))
-    }
-    var applyToken
-    if (!token || new Date((JSON.parse(atob(token.split('.')[1])).exp - 15 * 60 * 1000)) < Date.now()) {
-      applyToken = await refreshToken()
-    } else {
-      console.log(new Date((JSON.parse(atob(token.split('.')[1])).exp - 15 * 60 * 1000)), Date.now())
-    }
-    operation.setContext({
+    const applyToken = await getToken()
+    return {
       headers: {
+        ...headers,
         Authorization: `Bearer ${applyToken}`
       }
-    })
-    return forward(operation)
+    }
   } catch (e) {
     localStorage.removeItem('token')
-    console.error(e)
-    router.push('/naverAuth')
   }
 })
 
-// Name of the localStorage item'
-
-// Http endpoint
-
 const httpLink = new HttpLink({
-  // You should use an absolute URL here
   uri: process.env.VUE_APP_GRAPHQL_HTTP || 'https://trello.jeontuk-11.link/console/v1/graphql',
 });
 
-// Create the subscription websocket link
 const wsLink = new WebSocketLink({
   uri: process.env.VUE_APP_GRAPHQL_WS || 'wss://trello.jeontuk-11.link/console/v1/graphql',
   options: {
     reconnect: true,
-    lazy: true
+    lazy: true,
+    connectionParams: async () => {
+      const applyToken = await getToken()
+      return {
+        headers: {
+          Authorization: `Bearer ${applyToken}`
+        }
+      }
+    }
   },
 });
 
-// using the ability to split links, you can send data to each link
-// depending on what kind of operation is being sent
 const link = split(
-  // split based on operation type
   ({ query }) => {
-    console.log('link called')
     const definition = getMainDefinition(query)
     return definition.kind === 'OperationDefinition' &&
       definition.operation === 'subscription'
@@ -88,9 +76,8 @@ const link = split(
   httpLink,
 );
 
-// Create the apollo client
 const apolloClient = new ApolloClient({
-  link: from([authMiddleware, link]),
+  link: authLink.concat(link),
   cache: new InMemoryCache(),
   connectToDevTools: true,
 })
